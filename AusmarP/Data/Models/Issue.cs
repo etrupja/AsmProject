b1a6242757dfd394918fea4be9825d662f2e15f4
enian <eniantafa@gmail.com>
2018-02-13 13:17:13 +0100

    First Commit
    Mz First Commit

---
+++ AusmarP/Data/Models/Issue.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AusmarP.Data.Models
+{
+    public class Issue
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+
+        public virtual Site Site { get; set; }
+        public int SiteId { get; set; }
+
+    }
+}
---
+++ AusmarP/Data/Models/Site.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace AusmarP.Data.Models
+{
+    public class Site
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+
+        //Relations
+        public List<Issue> Issues { get; set; }
+
+
+    }
+}
